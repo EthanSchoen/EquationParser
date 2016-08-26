b9b70e56840668011ac94ded1c296b54993b417f
Ethan Schoen <e.schoen@yahoo.com>
2016-08-26 15:44:56 -0400

    numbers now clumped togeather; tree works?

--- parser.cpp
+++ parser.cpp
@@ -13,9 +13,9 @@ class Tree
 		Tree(std::string s)
 		{
 			nodeData = s;
-			Tree *parentNode = 0;
-			Tree *leftNode = 0;
-			Tree *rightNode = 0;
+			parentNode = 0;
+			leftNode = 0;
+			rightNode = 0;
 		}
 
 		Tree * addLeftNode(std::string s)
@@ -41,36 +41,54 @@ class Tree
 
 		int evaulate()
 		{
-			if(parentNode == 0)
-			{
+			if(parentNode == 0){
 				//perform acts of magic and wonderment
 				return 1;
-			} else
-			{
+			}else{
 				std::cout<<"not parent node, something wrong\n";
 				return 0; //dunno what to do for returning here
+			}
+		}
+
+		void checkAndPrint()
+		{
+			if(parentNode != 0){
+				printf("not parentNode\n");
+				return;
+			}
+			std::cout<<"parent: ";
+			printTree(*this);
+		}
+
+		void printTree(Tree t)
+		{
+			std::cout<<t.nodeData<<"\n";
+			if(t.leftNode != 0){
+				std::cout<<"left: ";
+				printTree(*(t.leftNode));
+			}
+			if(t.rightNode != 0){
+				std::cout<<"right: ";
+				printTree(*(t.rightNode));
 			}
 		}
 };
 
 //Initially parser will only do the opperations: + - * / and ()
-void numberState(char x, bool *error)
+Tree * symbolState(char x, bool *error)
 {
-	std::cout<<"number state and char is: "<<x<<std::endl;
+	std::cout<<x<<"\n";
 }
 
-void symbolState(char x, bool *error)
+int main()
 {
-	std::cout<<"symbol state and char is: "<<x<<std::endl;
-}
-
-int main(){
 	std::string eq;
 	std::cout<<"enter equation less than 100 characters with no spaces\n";
 	std::cin>>eq;
 	bool error = (eq[0] > 47 && eq[0] < 58) || eq[0] == '(' ? 0 : 1;//make sure equation starts right
 	if(error)
 		std::cout<<"equation must start with a number or a (\n";
+	std::string number;
 	int currentState;
 	int i = 0;
 	//FSM loop
@@ -103,11 +121,18 @@ int main(){
 			switch(currentState)
 			{
 				case 0:
-					numberState(eq[i], &error);
+				{
+					char temp = eq[i];
+					number.append(&temp);
 					break;
+				}
 				case 1:
+				{
+					std::cout<<number<<std::endl;
 					symbolState(eq[i], &error);
+					number = "";
 					break;
+				}
 			}
 		}
 		i++;
